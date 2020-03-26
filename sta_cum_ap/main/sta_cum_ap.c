96f1bd5d1eb3276dee4bf5003f84ec79060328ec
rocheparadox <rocheinside@gmail.com>
2020-03-26 07:42:16 +0530

    memcpy instead of strcpy, variables initialised during runtime instead of compilation time -- no define keyword

--- sta_cum_ap/main/sta_cum_ap.c
+++ sta_cum_ap/main/sta_cum_ap.c
@@ -1,7 +1,5 @@
 /* Simple WiFi Example
-
    This example code is in the Public Domain (or CC0 licensed, at your option.)
-
    Unless required by applicable law or agreed to in writing, this
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
@@ -16,17 +14,6 @@
 #include "esp_log.h"
 #include "nvs_flash.h"
 
-/* The examples use simple WiFi configuration that you can set via
-   'make menuconfig'.
-
-   If you'd rather not, just change the below entries to strings with
-   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
-*/
-#define STA_SSID   "Paradox"
-#define STA_PASS   "Nikola@1"
-
-#define AP_SSID		"gra77h0pper"
-#define AP_PASS		"experimental001"
 #define AP_MAX_CONN	10
 
 /* FreeRTOS event group to signal when we are connected*/
@@ -39,59 +26,66 @@ const int WIFI_CONNECTED_BIT = BIT0;
 
 static const char *TAG = "station cum access_point";
 
+int get_length(uint8_t*);
+
 static esp_err_t event_handler(void *ctx, system_event_t *event)
 {
     /* For accessing reason codes in case of disconnection */
     system_event_info_t *info = &event->event_info;
 
     switch(event->event_id) {
-    case SYSTEM_EVENT_STA_START:
-        esp_wifi_connect();
-        break;
-    case SYSTEM_EVENT_STA_GOT_IP:
-        ESP_LOGI(TAG, "got ip:%s",
-                 ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
-        xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
-        break;
-    case SYSTEM_EVENT_AP_STACONNECTED:
-        ESP_LOGI(TAG, "station:"MACSTR" join, AID=%d",
-                 MAC2STR(event->event_info.sta_connected.mac),
-                 event->event_info.sta_connected.aid);
-        break;
-    case SYSTEM_EVENT_AP_STADISCONNECTED:
-        ESP_LOGI(TAG, "station:"MACSTR" leave, AID=%d",
-                 MAC2STR(event->event_info.sta_disconnected.mac),
-                 event->event_info.sta_disconnected.aid);
-        break;
-    case SYSTEM_EVENT_STA_DISCONNECTED:
-        ESP_LOGE(TAG, "Disconnect reason : %d", info->disconnected.reason);
-        if (info->disconnected.reason == WIFI_REASON_BASIC_RATE_NOT_SUPPORT) {
-            /*Switch to 802.11 bgn mode */
-            esp_wifi_set_protocol(ESP_IF_WIFI_STA, WIFI_PROTOCAL_11B | WIFI_PROTOCAL_11G | WIFI_PROTOCAL_11N);
-        }
-        esp_wifi_connect();
-        xEventGroupClearBits(wifi_event_group, WIFI_CONNECTED_BIT);
-        break;
-    default:
-        break;
+        case SYSTEM_EVENT_STA_START:
+            esp_wifi_connect();
+            break;
+        case SYSTEM_EVENT_STA_GOT_IP:
+            ESP_LOGI(TAG, "got ip:%s",
+                     ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
+            xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
+            break;
+        case SYSTEM_EVENT_AP_STACONNECTED:
+            ESP_LOGI(TAG, "station:"MACSTR" join, AID=%d",
+                     MAC2STR(event->event_info.sta_connected.mac),
+                     event->event_info.sta_connected.aid);
+            break;
+        case SYSTEM_EVENT_AP_STADISCONNECTED:
+            ESP_LOGI(TAG, "station:"MACSTR" leave, AID=%d",
+                     MAC2STR(event->event_info.sta_disconnected.mac),
+                     event->event_info.sta_disconnected.aid);
+            break;
+        case SYSTEM_EVENT_STA_DISCONNECTED:
+            ESP_LOGE(TAG, "Disconnect reason : %d", info->disconnected.reason);
+            if (info->disconnected.reason == WIFI_REASON_BASIC_RATE_NOT_SUPPORT) {
+                /*Switch to 802.11 bgn mode */
+                esp_wifi_set_protocol(ESP_IF_WIFI_STA, WIFI_PROTOCAL_11B | WIFI_PROTOCAL_11G | WIFI_PROTOCAL_11N);
+            }
+            esp_wifi_connect();
+            xEventGroupClearBits(wifi_event_group, WIFI_CONNECTED_BIT);
+            break;
+        default:
+            break;
     }
     return ESP_OK;
 }
 
-void wifi_init_softap()
+void start_an_access_point(uint8_t* access_point_ssid, uint8_t* access_point_password)
 {
+
+    ESP_LOGI(TAG, "Gonna create an access point with creds SSID=%s, PASSWORD=%s", access_point_ssid, access_point_password);
 
     //Create a wifi configuration type variable to provide details for it to act as an access point
     wifi_config_t wifi_config = {
-        .ap = {
-            .ssid = AP_SSID,
-            .ssid_len = strlen(AP_SSID),
-            .password = AP_PASS,
-            .max_connection = AP_MAX_CONN,
-            .authmode = WIFI_AUTH_WPA_WPA2_PSK
-        },
+            .ap = {
+                    .max_connection = AP_MAX_CONN,
+                    .authmode = WIFI_AUTH_WPA_WPA2_PSK
+            },
     };
-    if (strlen(AP_PASS) == 0) {
+
+    uint8_t ssid_length = get_length(access_point_ssid);
+    memcpy(wifi_config.sta.ssid, access_point_ssid, get_length(access_point_ssid));
+    memcpy(wifi_config.sta.password, access_point_password, get_length(access_point_password));
+    wifi_config.ap.ssid_len=ssid_length;
+
+    if (strlen((char*)access_point_password) == 0) {
         //When you do not care about people peeping to your network, do this.
         wifi_config.ap.authmode = WIFI_AUTH_OPEN;
     }
@@ -101,19 +95,24 @@ void wifi_init_softap()
     ESP_ERROR_CHECK(esp_wifi_start());
 
     ESP_LOGI(TAG, "wifi_init_softap finished.SSID:%s password:%s",
-             AP_SSID, AP_PASS);
+             access_point_ssid, access_point_password);
 }
 
-void wifi_init_sta()
+void connect_to_access_point(uint8_t* ssid, uint8_t* password)
 {
+
+    ESP_LOGI(TAG, "Gonna connect to the access point with creds SSID=%s, PASSWORD=%s", ssid, password);
 
     //Create a wifi configuration type variable to provide details of the access point to which it has to connect
     wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = STA_SSID,
-            .password = STA_PASS
-        },
+            .sta ={},
     };
+
+    memcpy(wifi_config.sta.ssid, ssid, get_length(ssid));
+    memcpy(wifi_config.sta.password, password, get_length(password));
+
+    //strcpy((char*)wifi_config.sta.ssid, (char*)ssid);
+    //strcpy((char*)wifi_config.sta.password, (char*)password);
 
     //ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
     //Set mode to WIFI_MODE_APSTA, so it can act as both station and access point
@@ -123,16 +122,38 @@ void wifi_init_sta()
 
     ESP_LOGI(TAG, "wifi_init_sta finished.");
     ESP_LOGI(TAG, "connect to ap SSID:%s password:%s",
-             STA_SSID, STA_PASS);
+             ssid, password);
+}
+
+int get_length(uint8_t *str){
+    int len=0;
+    //printf("\nstring is %s", str);
+    while(str[len] != 0){
+        //printf("\nchar is %c", str[len]);
+        len++;
+    }
+
+    //return sizeof(str);
+    //Add one for null termination
+    return len+1;
 }
 
 void app_main()
 {
+    //Initialize the variables
+    uint8_t ssid_to_connect[32]="reverance";
+    uint8_t password4ssid_to_connect[64]="edwardsnowden";
+
+    uint8_t access_point_ssid[32]="gra77h0pp3r";
+    uint8_t access_point_password[64]="experimental001";
+
+
+
     //Initialize NVS
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      ret = nvs_flash_init();
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
 
@@ -157,11 +178,11 @@ void app_main()
      * for it to be stable.
     */
 
-    ESP_LOGI(TAG, "Connect to the Access point as a staion");
-    wifi_init_sta();
+    ESP_LOGI(TAG, "Connect to the Access point as a station ");
+    connect_to_access_point(ssid_to_connect, password4ssid_to_connect);
 
 
     ESP_LOGI(TAG, "Create an access point");
-    wifi_init_softap();
+    start_an_access_point(access_point_ssid, access_point_password);
 
 }
